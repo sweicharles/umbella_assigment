05bbc9645d853fdd13d2edd8aa51ebfaa8b39379
CharlesSwei <sweicharles@gmail.com>
2020-05-11 02:42:03 +1000

    finalized

--- JS/theUmbella.js
+++ JS/theUmbella.js
@@ -24,30 +24,30 @@ $(document).ready(function(){
 
 
 
-const btn = document.getElementById("test");
-btn.addEventListener("click", (event)=> {
-  event.preventDefault();
-  console.log("the item has been added to the cart")
-})
+// const btn = document.getElementById("test");
+// btn.addEventListener("click", (event)=> {
+//   event.preventDefault();
+//   console.log("the item has been added to the cart")
+// })
 
 
 // back to top botton
 //Get the button:
-mybutton = document.getElementById("myBtn");
+// mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
