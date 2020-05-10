e002ad7e18f385d1e12b9e344e4543bac5adbfd9
CharlesSwei <sweicharles@gmail.com>
2020-05-10 21:16:44 +1000

    rebuild the record

---
+++ JS/theUmbella.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+  // Activate Carousel with a specified interval
+  $("#myCarousel").carousel({interval: 5000});
+        
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+    
+  // Enable Carousel Controls
+  $(".carousel-control-prev").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".carousel-control-next").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
+
+
+
+const btn = document.getElementById("test");
+btn.addEventListener("click", (event)=> {
+  event.preventDefault();
+  console.log("the item has been added to the cart")
+})
+
+
+// back to top botton
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
