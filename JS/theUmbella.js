023d9351bec43866e578ce9e7a92cf107c87ac22
CharlesSwei <sweicharles@gmail.com>
2020-05-13 21:03:07 +1000

    add music back

--- JS/theUmbella.js
+++ JS/theUmbella.js
@@ -22,32 +22,5 @@ $(document).ready(function(){
   });
 });
 
-
-
-// const btn = document.getElementById("test");
-// btn.addEventListener("click", (event)=> {
-//   event.preventDefault();
-//   console.log("the item has been added to the cart")
-// })
-
-
-// back to top botton
-//Get the button:
-// mybutton = document.getElementById("myBtn");
-
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function () { scrollFunction() };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
+var music = document.getElementById("myVideo");
+music.volume = 0.2;
--- JS/theUmbella_product.js
+++ JS/theUmbella_product.js
@@ -44,3 +44,6 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+var music = document.getElementById("myVideo");
+music.volume = 0.2;
